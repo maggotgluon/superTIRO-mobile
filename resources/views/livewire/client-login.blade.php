915774c4c7ae46566b59a45fb445a090f05f2c02
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-03-21 11:20:58 +0700

    Removed link

--- resources/views/livewire/client-login.blade.php
+++ resources/views/livewire/client-login.blade.php
@@ -13,7 +13,7 @@
     class="{{$error!=''?'ring-secondary-red':''}}"/>
 
     <div class="py-2 text-center mt-auto flex justify-between items-center">
-        <a class="text-secondary-red hover:text-primary-blue transition-all duration-500" href="{{route('client.register')}}">ยังไม่ได้ลงทะเบียนรับสิทธิ์</a>
+       
         <x-button lg right-icon="chevron-right" primary class="bg-gradient-to-br from-gradient-start to-gradient-end rounded-2xl" 
         wire:click="login" type="button" label="ตรวจสอบ" />
     </div>
