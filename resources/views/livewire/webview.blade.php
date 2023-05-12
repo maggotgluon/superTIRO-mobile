7aa835b0e826ff8587548134e0037b18ed663fec
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-12 15:17:27 +0700

    webview test

--- resources/views/livewire/webview.blade.php
+++ resources/views/livewire/webview.blade.php
@@ -1,4 +1,12 @@
 <div>
+    @if(strpos($_SERVER['HTTP_USER_AGENT'], 'wv') !== false)
+        Mobile Web View
+    @elseif( strpos($_SERVER['HTTP_USER_AGENT'], 'Mobile') !== false)
+        Mobile
+    @else
+        Other
+    @endif
+
     You are using {{$device}}<br>
     {!!$browser!!}
     <ul>
