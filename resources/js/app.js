c13e4a8a3962969a5b694ab8199ed10108ce18af
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-13 22:14:40 +0700

    no message

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,8 +6,11 @@ window.Alpine = Alpine;
 
 Alpine.start();
 const inapp = new InApp(navigator.userAgent || navigator.vendor || window.opera);
-if(inapp.isInApp && window.location.href!=='http://127.0.0.1:8000/view'){
-    window.location.replace('http://127.0.0.1:8000/view');
+if(inapp.isInApp){
+    alert('web view detect');
+    if(window.location.href!=='http://127.0.0.1:8000/view'){
+        window.location.replace('http://127.0.0.1:8000/view');
+    }
 }else{
     console.log(inapp.browser);
 }
