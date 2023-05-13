4108a8f24cf31c5ac87a04844f02b3993bb0c198
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-13 23:02:39 +0700

    no message

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,11 +10,11 @@ Alpine.start();
 const inapp = new InApp(navigator.userAgent || navigator.vendor || window.opera);
 
 if(inapp.isInApp){
-    alert('web view detect');
-    if(window.location.href!=='http://127.0.0.1:8000/view'){
-        window.location.replace('http://127.0.0.1:8000/view');
+    // alert('web view detect');
+    if(window.location.href!=='https://consultationprogram.supertriodog.com/view'){
+        window.location.replace('https://consultationprogram.supertriodog.com/view');
     }
 }else{
-    alert(inapp.browser);
+    // alert(inapp.browser);
     console.log(inapp.browser);
 }
