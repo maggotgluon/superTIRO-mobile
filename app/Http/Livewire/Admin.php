7d7bfd8a839b91712ca32bbe93fe505031728ed8
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-13 07:34:50 +0700

    update admin dropdown

--- app/Http/Livewire/Admin.php
+++ app/Http/Livewire/Admin.php
@@ -6,6 +6,8 @@ use App\Models\Vet;
 use Livewire\Component;
 use Illuminate\Support\Facades\Auth;
 
+use Illuminate\Support\Facades\DB;
+
 class Admin extends Component
 {
     public $vet_list,$VetSelect;
@@ -19,7 +21,9 @@ class Admin extends Component
     public function mount(){
         // $vet_all = Vet::lazy();
         // dd($vet_all);
-        foreach (Vet::lazy() as $index => $vet) {
+        $v= DB::table('vets')->get();
+        
+        foreach ($v as $index => $vet) {
             $this->vet_list[$index]['id']=$vet->id;
             $this->vet_list[$index]['name']=$vet->vet_name;
             $this->vet_list[$index]['description']=$vet->vet_area.' '.$vet->vet_city.' '.$vet->vet_province;
--- resources/views/livewire/admin.blade.php
+++ resources/views/livewire/admin.blade.php
@@ -3,10 +3,11 @@
 
     <x-button flat label="dashboard" icon="template" href="{{route('admin.dashboard')}}" />
     <x-button flat label="Vet" icon="shopping-cart" href="{{route('admin.vets')}}" />
-
-    <x-native-select class="py-4 w-24 ml-auto sm:w-auto" 
+    
+    <x-select class="py-4 ml-auto" 
     placeholder="ค้นหาชื่อคลินิก" :options="$vet_list" option-label="name"
-     option-value="id" wire:model="VetSelect" />
+    option-value="id" wire:model="VetSelect" />
+    
     <x-dropdown>
         <x-slot name="trigger">
             <x-button.circle icon="user" label="Options" primary />
