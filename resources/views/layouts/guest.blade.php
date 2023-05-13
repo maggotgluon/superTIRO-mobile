a79c5b5909bbb380200c1b168b14a2f1641ffcfa
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-13 19:30:27 +0700

    Update guest.blade.php

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -61,19 +61,7 @@
                     {{ $slot }}
                 </div>
             </div>
-        <!-- else
-            <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-300">
-                <div>
-                    <a href="/">
-                        <x-application-logo class="h-20 fill-current text-gray-500" />
-                    </a>
-                </div>
-    
-                <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
-                    {{ $slot }}
-                </div>
-            </div>
-        endif -->
+        
         @livewireScripts
     </body>
 </html>
