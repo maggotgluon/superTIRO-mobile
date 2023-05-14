f176e3af1c7cc21885ef7093a7b8c4ecbac4d2ca
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-14 11:48:08 +0700

    no message

--- routes/api.php
+++ routes/api.php
@@ -20,11 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::get('/vets',function(){
 
     foreach (DB::table('vets')->get() as $index => $vet) {
-        // dd($vet->user_id);
         $vet_list[$index]['id']=$vet->id;
         $vet_list[$index]['name']=$vet->user_id.' '.$vet->vet_name;
         $vet_list[$index]['description']=$vet->vet_area.' '.$vet->vet_city.' '.$vet->vet_province;
     }
-    // dd($vet_list);
     return $vet_list;
 })->name('vets');
