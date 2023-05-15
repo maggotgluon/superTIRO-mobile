35def303466a4d0abb7ba6a4ced7b470ef4fdf27
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-15 22:08:12 +0700

    uplog config

--- config/logging.php
+++ config/logging.php
@@ -53,7 +53,7 @@ return [
     'channels' => [
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single','daily','syslog','errorlog'],
             'ignore_exceptions' => false,
         ],
 
