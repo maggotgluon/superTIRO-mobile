e5f581a1e4aeb399ef0bed52c2af405ed5664ad9
Ruttaphong Worachartudomphong <maggotgluon@gmail.com>
2023-05-17 08:44:40 +0700

    Update admin.blade.php

--- resources/views/livewire/admin.blade.php
+++ resources/views/livewire/admin.blade.php
@@ -5,7 +5,8 @@
     <x-button flat label="Vet" icon="shopping-cart" href="{{route('admin.vets')}}" />
     <x-select class="py-4 ml-auto w-auto" 
     :options="$vet_list"
-    searchable=false
+    searchable="false"
+    min-items-for-search=10000
     option-label="name"
     option-value="id"
     placeholder="ค้นหาชื่อคลินิก" wire:model="VetSelect" 
