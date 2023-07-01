c7fc20a0bb30dc473679911c3a99f4e4abf62af2
Ruttaphong <maggotgluon@gmail.com>
2023-07-01 12:21:32 +0700

    adj font

--- resources/views/under.blade.php
+++ resources/views/under.blade.php
@@ -1,3 +1,4 @@
 <x-guest-layout>
-    <h1 class="text-2xl font-bold">ขออภัย โปรแกรมนี้หมดเขตลงทะเบียนแล้ว</H1>
+    <h1 class="text-3xl font-bold font-sans text-center">ขออภัย </h1>
+    <h2 class="text-2xl font-bold font-sans text-center">โปรแกรมนี้หมดเขตลงทะเบียนแล้ว</H2>
 </x-guest-layout>
